539d764ed1449976ef902af05cd749b71d464e8a
Wade <40057831+WadeStack@users.noreply.github.com>
2020-12-04 14:51:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 import os
 
- 
+data = {
+    'site_name': 'My Docs',
+    'theme': 'readthedocs',
+    'plugins': ['search', ],
+    'pages': ['index: index.md']
+}
+cur_path = os.getcwd()
+ul = os.listdir(os.path.join(cur_path, 'docs'))
 
-print(os.getcwd())
+data['pages'] = ul
+with open('mkdocs.yml', 'w+', encoding='utf-8') as f:
+    for k, vs in data.items():
+
+        if isinstance(vs, list):
+            f.write('{}:\n'.format(k))
+            for v in vs:
+                f.write('    - {}\n'.format(v))
+
+        else:
+            f.write('{}: {}\n'.format(k, vs))
